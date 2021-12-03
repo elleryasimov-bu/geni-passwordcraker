b2ed10506faa54f1c7a332f031f2a71afe9ed07a
Kaijie Chen <kaijiec@bu.edu>
2021-12-03 17:18:33 -0500

    service package added

---
+++ master/src/main/java/org/geniprojects/passwordcracker/master/service/Range.java
@@ -0,0 +1,18 @@
+package org.geniprojects.passwordcracker.master.service;
+
+public class Range {
+    public String leftBound;
+    public String rightBound;
+    public Range() {
+        leftBound = "";
+        rightBound = "";
+    }
+    public Range(String leftBound, String rightBound) throws Exception{
+        if (leftBound.compareTo(rightBound) > 0) {
+            throw new Exception("left > right");
+        } else {
+            this.leftBound = leftBound;
+            this.rightBound = rightBound;
+        }
+    }
+}
---
+++ master/src/main/java/org/geniprojects/passwordcracker/master/service/ResourceRetriever.java
@@ -0,0 +1,4 @@
+package org.geniprojects.passwordcracker.master.service;
+
+public class ResourceRetriever {
+}
