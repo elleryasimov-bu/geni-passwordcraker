0c46e17c9da3ae61df2e151166371459b54b2f3a
ZimouSun <zimousun@bu.edu>
2021-12-03 17:29:19 -0500

    StringSpliter modified

--- master/src/main/java/org/geniprojects/passwordcracker/master/service/StringSpliter.java
+++ master/src/main/java/org/geniprojects/passwordcracker/master/service/StringSpliter.java
@@ -2,30 +2,31 @@ package org.geniprojects.passwordcracker.master.service;
 
 import java.security.*;
 import java.math.*;
+import java.util.ArrayList;
 
-public class StringSpliter {  
-  
-   public static String[] splitStrforWorkers(int workerNum,String inputString)
+public class StringSpliter {
+
+   public static ArrayList<Range> splitStrforWorkers(int workerNum,String inputString)throws Exception
    {
       String rangeStr =inputString;
 
       int numOfWorker=workerNum;
-     
+
       // divide by worker number
       int quotient = rangeStr.length()/numOfWorker;
       int remainder = rangeStr.length()%numOfWorker;
-      
+
       int counter =0;
-     
+
       String divided_subString ="";
       for (int i =0; i<rangeStr.length();i++)
       {
          if(counter==0)
-         divided_subString+=rangeStr.charAt(i)+"AAAA->";
+            divided_subString+=rangeStr.charAt(i)+"AAAA->";
 
          counter++;
-         
-         if(counter ==quotient) 
+
+         if(counter ==quotient)
          {
             if(remainder>0) // distribute one unit of work from "remainder" to worker,  load balancing
             {
@@ -44,23 +45,16 @@ public class StringSpliter {
          }
       }
       String[] arrOfStr = divided_subString.split(" ", numOfWorker);
-      return arrOfStr;
-   }
-
-   public static void main(String args[])  {     
-      // //Encode     
-      // String md5 = MD5("MD5Online");     
-      // System.out.println("MD5 hash: "+md5);
-
-
-      
-      
-      int numOfWorker =5;
-
-      for (String s :splitStrforWorkers(numOfWorker,"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz") )
+      ArrayList<Range> workDistributionRange = new ArrayList<Range>();
+      for (String s :arrOfStr )
       {
-         System.out.println(s);
-      }
+         String[] temp=s.split("->",2);
 
+         workDistributionRange.add(new Range(temp[0],temp[1]) );
+
+      }
+      return workDistributionRange;
    }
+
+
 }
