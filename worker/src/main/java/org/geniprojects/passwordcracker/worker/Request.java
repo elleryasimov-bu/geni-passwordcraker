101bff8257e657f331b745f9316fcd10434ada4c
elleryasimov-bu <71101123+elleryasimov-bu@users.noreply.github.com>
2021-12-03 20:42:29 -0500

    Request updated

--- worker/src/main/java/org/geniprojects/passwordcracker/worker/Request.java
+++ worker/src/main/java/org/geniprojects/passwordcracker/worker/Request.java
@@ -4,12 +4,18 @@ import java.io.Serializable;
 
 public class Request implements Serializable {
     public String enCryptedString;
+    //AAAAA
+    public String leftBound;
+    //zzzzz
+    public String rightBound;
 
     public Request() {
         this.enCryptedString = "";
     }
-    public Request(String enCryptedString) {
+    public Request(String enCryptedString, String leftBound, String rightBound) {
         this.enCryptedString = enCryptedString;
+        this.leftBound = leftBound;
+        this.rightBound = rightBound;
     }
 
 
